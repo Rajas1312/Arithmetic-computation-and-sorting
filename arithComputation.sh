9e1e26597baad7e5993bd87278a7118fe2461109
Rajas1312 <itsmerajas2@gmail.com>
2020-11-03 13:56:33 +0530

    taking three inputs from user

--- arithComputation.sh
+++ arithComputation.sh
@@ -2,3 +2,13 @@
 
 echo "Welcome to Arithmetic computation and sorting"
 
+echo "Enter number 1-"
+read a
+echo $a
+echo "Enter number 2-"
+read b
+echo $b
+echo "enter number 3-"
+read c
+echo $c
+
